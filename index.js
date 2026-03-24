c34ae2e9784bd190478a01746242a40b426b4d24
MrPravinS <shegamwarpravin07@gmail.com>
2026-03-24 11:53:09 +0530

    updated

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get("/",(req,res)=>{
     res.send({msg:"Hey from EC2"})
 })
 
+app.get("/:name",(req,res)=>{
+    const name = req.params.name;
+
+    if(name=== "sharvari"){
+        res.send("hey Baby ")
+    }
+    console.log(name);
+    res.send({msg:`hello ${name}`})
+    
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT:${PORT}`);
     
