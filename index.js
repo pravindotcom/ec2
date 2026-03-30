c3be76c6792b24bce749f6b9f7b9eae3be492851
MrPravinS <shegamwarpravin07@gmail.com>
2026-03-30 23:59:47 +0530

    fixed

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ app.get("/",(req,res)=>{
     res.send({msg:"Hey from EC2"})
 })
 
-app.get("/:name",(req,res)=>{
-    const name = req.params.name;
-    console.log(name);
-    res.send({msg:`hello ${name}`})
-    
-})
+
 
 app.get("/contact",(req,res)=>{
     res.send({message:"This is contact page"})
