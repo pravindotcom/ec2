379f18f2f6a87ff5b6e07318c632c023c9fd9496
MrPravinS <shegamwarpravin07@gmail.com>
2026-04-03 00:18:58 +0530

    added load balancer

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ import express from "express"
 const app = express();
 const PORT = 8080
 
-app.get("/",(req,res)=>{
-    res.send({msg:"Hey from EC2"})
+aapp.get("/", (req, res) => {
+  res.send("Welcome to AWS tutorial by Shivam Bhadani");
 })
 
+app.get("/product", (req, res) => {
+  res.send("This is Product Page");
+})
 
-
-app.get("/contact",(req,res)=>{
-    res.send({message:"This is contact page"})
+app.get("/health", (req, res) => {
+  res.send("Everything is OK");
 })
 
 app.listen(PORT,()=>{
